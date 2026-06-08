3ad70f2600434a464b9851b7a6cf460ae75de2e2
loulima240 <lourencollima2006@gmail.com>
2026-06-08 21:41:59 +0100

    Revert "Merge pull request #1 from lucasvcasimiro-byte/lima"
    This reverts commit 9f868e5d580950f8b4034ff184e8f93bf8001233, reversing
    changes made to e1ea5e37d1abba6512ea9668b36e6d6b635bcb9e.

--- scratch/inspect_basket.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df_basket = pd.read_csv("data/customer_basket.csv", nrows=5)
-print("Basket columns:", df_basket.columns.tolist())
-print(df_basket.head())
--- scratch/inspect_clustering_cols.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv("data/ci_clustering.csv", nrows=5)
-print("ci_clustering columns:", df.columns.tolist())
